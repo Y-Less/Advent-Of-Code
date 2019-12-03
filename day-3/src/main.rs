86fb916124e006ed325f28fb95b7549c25796e40
Y_Less <git@y-less.com>
2019-12-03 12:20:45 +0100

    Day 3.  Star 1.

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -46,41 +46,90 @@ fn get_direction(i: &str) -> Direction //Result<Direction, &'static str>
 
 fn insert_all(cells: &mut HashSet<Cell>, a: Cell, b: Cell) -> Cell
 {
-	if a.0 == b.0
+	if a.1 < b.1
 	{
-		if a.1 < b.1
+		for y in a.1 .. b.1
 		{
-			for y in a.1 .. b.1
-			{
-				cells.insert((a.0, y + 1));
-			}
+			cells.insert((a.0, y + 1));
 		}
-		if a.1 > b.1
+	}
+	if a.1 > b.1
+	{
+		for y in b.1 .. a.1
 		{
-			for y in b.1 .. a.1
+			cells.insert((a.0, y));
+		}
+	}
+	if a.0 < b.0
+	{
+		for x in a.0 .. b.0
+		{
+			cells.insert((x + 1, a.1));
+		}
+	}
+	if a.0 > b.0
+	{
+		for x in b.0 .. a.0
+		{
+			cells.insert((x, a.1));
+		}
+	}
+	b
+}
+
+fn check_all(cells: &HashSet<Cell>, intersects: &mut HashSet<Cell>, a: Cell, b: Cell) -> Cell
+{
+	if a.1 < b.1
+	{
+		for y in a.1 .. b.1
+		{
+			if cells.contains(&(a.0, y + 1))
 			{
-				cells.insert((a.0, y));
+				intersects.insert((a.0, y + 1));
 			}
 		}
 	}
-	if a.1 == b.1
+	if a.1 > b.1
 	{
-		if a.0 < b.0
+		for y in b.1 .. a.1
 		{
-			for x in a.0 .. b.0
+			if cells.contains(&(a.0, y))
 			{
-				cells.insert((x + 1, a.1));
+				intersects.insert((a.0, y));
 			}
 		}
-		if a.0 > b.0
+	}
+	if a.0 < b.0
+	{
+		for x in a.0 .. b.0
 		{
-			for x in b.0 .. a.0
+			if cells.contains(&(x + 1, a.1))
 			{
-				cells.insert((x, a.1));
+				intersects.insert((x + 1, a.1));
+			}
+		}
+	}
+	if a.0 > b.0
+	{
+		for x in b.0 .. a.0
+		{
+			if cells.contains(&(x, a.1))
+			{
+				intersects.insert((x, a.1));
 			}
 		}
 	}
 	b
+}
+
+//fn distance(a: Cell, b: Cell) -> i32
+//{
+//	(a.0 - b.0).abs() + (a.1 - b.1).abs()
+//}
+
+fn distance(a: &Cell) -> i32
+{
+	a.0.abs() + a.1.abs()
 }
 
 fn main()
@@ -89,12 +138,12 @@ fn main()
 	
 	let stdin = io::stdin();
 	
-	let mut input = String::new();
+	let mut i0 = String::new();
 	
-	stdin.read_line(&mut input)
+	stdin.read_line(&mut i0)
 		.expect("Please enter the first wire");
 	
-	let wire1 = input.trim().split(',');
+	let wire1 = i0.trim().split(',');
 
 	let mut cells: HashSet<Cell> = HashSet::new();
 	
@@ -107,17 +156,33 @@ fn main()
 		{
 		Direction::U { n } => pos = insert_all(&mut cells, pos, (pos.0, pos.1 + n)),
 		Direction::D { n } => pos = insert_all(&mut cells, pos, (pos.0, pos.1 - n)),
-		Direction::L { n } => pos = insert_all(&mut cells, pos, (pos.0 + n, pos.1)),
-		Direction::R { n } => pos = insert_all(&mut cells, pos, (pos.0 - n, pos.1)),
+		Direction::L { n } => pos = insert_all(&mut cells, pos, (pos.0 - n, pos.1)),
+		Direction::R { n } => pos = insert_all(&mut cells, pos, (pos.0 + n, pos.1)),
 		}
 	});
-	cells.iter().for_each(print_position);
+	//cells.iter().for_each(print_position);
 	
-	
-	//wire1.for_each(|x| match x[0]
-	//{
-	//	'U;
-	//
-	//})
+	let mut i1 = String::new();
+
+	stdin.read_line(&mut i1)
+		.expect("Please enter the second wire");
+
+	let wire2 = i1.trim().split(',');
+	pos = (0, 0);
+
+	let mut intersects: HashSet<Cell> = HashSet::new();
+
+	wire2.for_each(|x|
+	{
+		match get_direction(x)
+		{
+		Direction::D { n } => pos = check_all(&cells, &mut intersects, pos, (pos.0, pos.1 - n)),
+		Direction::U { n } => pos = check_all(&cells, &mut intersects, pos, (pos.0, pos.1 + n)),
+		Direction::L { n } => pos = check_all(&cells, &mut intersects, pos, (pos.0 - n, pos.1)),
+		Direction::R { n } => pos = check_all(&cells, &mut intersects, pos, (pos.0 + n, pos.1)),
+		}
+	});
+	intersects.remove(&(0, 0));
+	println!("Minimum: {:?}", intersects.iter().map(distance).min());
 }
 
