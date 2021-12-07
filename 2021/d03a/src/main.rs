507c6f83988b8f5ba5194b7fc19aba7b0e85e542
Y_Less <git-athena@y-less.com>
2021-12-07 01:54:55 +0000

    d03a

---
+++ 2021/d03a/src/main.rs
@@ -0,0 +1,59 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() -> std::io::Result<()>
+{
+	let mut input = String::new();
+	{
+		let mut file = File::open("../inputs/d03.txt")?;
+		file.read_to_string(&mut input)?;
+	}
+
+	let mut naughts: [i32; 5] = [0; 5];
+	let mut ones: [i32; 5] = [0; 5];
+	
+	let numbers = input.trim().split('\n')
+		.map(|x| i32::from_str_radix(x, 2).unwrap())
+		.collect::<Vec<i32>>();
+
+	println!("{:?}", numbers);
+
+	for n in numbers
+	{
+		for i in 0..5
+		{
+			let b = 1 << i;
+			if ((n & b) == 0)
+			{
+				naughts[i] = naughts[i] + 1;
+			}
+			else
+			{
+				ones[i] = ones[i] + 1;
+			}
+		}
+	}
+
+	let mut gamma = 0;
+	let mut epsilon = 0;
+	for i in 0..5
+	{
+		let b = 1 << i;
+		if (ones[i] > naughts[i])
+		{
+			gamma = gamma | b;
+		}
+		else
+		{
+			epsilon = epsilon | b;
+		}
+	}
+
+	println!("{:?}", gamma);
+	println!("{:?}", epsilon);
+	println!("{:?}", gamma * epsilon);
+
+
+	Ok(())
+}
+
