40bb137dcee59d8aaa26b025615491c5da293426
Y_Less <git-athena@y-less.com>
2021-12-07 01:10:42 +0000

    d01a initial code.

---
+++ 2021/d01a/src/main.rs
@@ -0,0 +1,34 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() -> std::io::Result<()>
+{
+	println!("Enter the program.");
+
+	let mut input = String::new();
+	{
+		let mut file = File::open("../inputs/d01.txt")?;
+		file.read_to_string(&mut input)?;
+	}
+
+	let numbers = input.trim().split('\n')
+		.map(|x| x.parse().expect("Not a number"))
+		.collect::<Vec<i32>>();
+
+	let mut prev = -1;
+	let mut count = -1;
+
+	for n in numbers
+	{
+		if (n > prev)
+		{
+			count = count + 1;
+		}
+		prev = n;
+	}
+
+	println!("{:?}", count);
+
+	Ok(())
+}
+
