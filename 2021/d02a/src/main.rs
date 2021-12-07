90228bc72ade028055e3914b56ea4ab0a644d35e
Y_Less <git-athena@y-less.com>
2021-12-07 01:33:47 +0000

    d02a initial code.

---
+++ 2021/d02a/src/main.rs
@@ -0,0 +1,57 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() -> std::io::Result<()>
+{
+	println!("Enter the program.");
+
+	let mut input = String::new();
+	{
+		let mut file = File::open("../inputs/d02.txt")?;
+		file.read_to_string(&mut input)?;
+	}
+
+	//let course = input.trim().split('\n')
+	//	.collect::<Vec<String>>();
+
+	let mut f = 0;
+	let mut d = 0;
+	let mut u = 0;
+	for i in input.trim().split('\n')
+	{
+		let mut iter = i.splitn(2, ' ');
+		let command = iter.next().unwrap();
+		let value: i32 = iter.next().unwrap().parse().expect("Not a number");
+		//println!("{:?}", command);
+		//println!("{:?}", value);
+		if (command == "forward")
+		{
+			f = f + value;
+		}
+		else if (command == "down")
+		{
+			d = d + value;
+		}
+		else if (command == "up")
+		{
+			u = u + value;
+		}
+	}
+	
+	//let mut prev = -1;
+	//let mut count = -1;
+    //
+	//for n in numbers
+	//{
+	//	if (n > prev)
+	//	{
+	//		count = count + 1;
+	//	}
+	//	prev = n;
+	//}
+
+	println!("{:?}", f * (d - u));
+
+	Ok(())
+}
+
