f94b210f82bc8ac38c707d75082c2c953c01d20f
Y_Less <git-athena@y-less.com>
2021-12-08 21:38:19 +0000

    Real input.

--- 2021/d05a/src/main.rs
+++ 2021/d05a/src/main.rs
@@ -22,7 +22,7 @@ fn main() -> std::io::Result<()>
 		{
 			continue;
 		}
-		println!("{:?}", line);
+		//println!("{:?}", line);
 		for i in std::cmp::min(parts[0], parts[2])..std::cmp::max(parts[0], parts[2]) + 1
 		{
 			for j in std::cmp::min(parts[1], parts[3])..std::cmp::max(parts[1], parts[3]) + 1
@@ -49,92 +49,10 @@ fn main() -> std::io::Result<()>
 					v.insert(j, 1);
 					vents.insert(i, v);
 				}
-				//if (!vents.contains_key(&i))
-				//{
-				//	vents[&i].insert(HashMap::new());
-				//}
-				//if (vents[&i].contains_key(&j))
-				//{
-				//	vents[&i][&j].insert(vents[&i][&j] + 1);
-				//}
-				//else
-				//{
-				//	vents[&i][&j] = 1;
-				//}
 			}
 		}
 	}
-	//println!("{:?}", vents);
-
-	//lines.next();
-	//let mut board: [[i32; 5]; 5] = [[0; 5]; 5];
-	//let mut i = 0;
-	//loop
-	//{
-	//	match lines.next()
-	//	{
-	//		Some(line) =>
-	//		{
-	//			//println!("{:?}", line.split_whitespace().collect::<Vec<&str>>());
-	//			
-	//			let numbers: Vec<i32> = line.split_whitespace()
-	//				.map(|x| x.parse().unwrap())
-	//				.collect::<Vec<i32>>();
-	//			if (numbers.len() == 0)
-	//			{
-	//				boards.push(board);
-	//				board = [[0; 5]; 5];
-	//				i = 0;
-	//			}
-	//			else
-	//			{
-	//				board[i][0] = numbers[0];
-	//				board[i][1] = numbers[1];
-	//				board[i][2] = numbers[2];
-	//				board[i][3] = numbers[3];
-	//				board[i][4] = numbers[4];
-	//				i = i + 1;
-	//			}
-	//		}
-	//		None =>
-	//		{
-	//			boards.push(board);
-	//			break;
-	//		}
-	//	}
-	//}
-	//
-	//let totalBoards = boards.len();
-	//let mut remainingBoards = totalBoards;
-	//let mut prev: Vec<i32> = Vec::new();
-	//for i in 1..calls.len()
-	//{
-	//	let mut part: Vec<i32> = Vec::new();
-	//	part.resize(i, 0);
-	//	part.clone_from_slice(&calls[..i]);
-	//	//println!("{:?}", part);
-	//	let mut completeBoards = 0;
-	//	for board in &boards
-	//	{
-	//		let sum = boardComplete(board, &part);
-	//		if (sum != -1)
-	//		{
-	//			completeBoards = completeBoards + 1;
-	//			if (remainingBoards == 1)
-	//			{
-	//				if (boardComplete(board, &prev) == -1)
-	//				{
-	//					let call = calls[i - 1];
-	//					println!("{:?}", board);
-	//					println!("{:?}", sum * call);
-	//					return Ok(());
-	//				}
-	//			}
-	//		}
-	//	}
-	//	prev = part;
-	//	remainingBoards = totalBoards - completeBoards;
-	//}
+	println!("{:?}", dangerous);
 	
 	Ok(())
 }
