3c078c47956a9512825b68ec365f19d01f7bf0f8
Y_Less <git-athena@y-less.com>
2021-12-08 16:05:49 +0000

    Evaluate the board and sum the remains (or return `-1` if it is incomplete).

--- 2021/d04a/src/main.rs
+++ 2021/d04a/src/main.rs
@@ -10,6 +10,47 @@ fn readNumbers(line: &str) -> Vec<i32>
 	numbers
 }
 
+fn boardComplete(board: [[i32; 5]; 5], numbers: Vec<i32>) -> i32
+{
+	if
+		(numbers.contains(&board[0][0]) && numbers.contains(&board[0][1]) && numbers.contains(&board[0][2]) && numbers.contains(&board[0][3]) && numbers.contains(&board[0][4])) ||
+		(numbers.contains(&board[1][0]) && numbers.contains(&board[1][1]) && numbers.contains(&board[1][2]) && numbers.contains(&board[1][3]) && numbers.contains(&board[1][4])) ||
+		(numbers.contains(&board[2][0]) && numbers.contains(&board[2][1]) && numbers.contains(&board[2][2]) && numbers.contains(&board[2][3]) && numbers.contains(&board[2][4])) ||
+		(numbers.contains(&board[3][0]) && numbers.contains(&board[3][1]) && numbers.contains(&board[3][2]) && numbers.contains(&board[3][3]) && numbers.contains(&board[3][4])) ||
+		(numbers.contains(&board[4][0]) && numbers.contains(&board[4][1]) && numbers.contains(&board[4][2]) && numbers.contains(&board[4][3]) && numbers.contains(&board[4][4]))
+	{
+		// Horizontals.
+	}
+	else if
+		(numbers.contains(&board[0][0]) && numbers.contains(&board[1][0]) && numbers.contains(&board[2][0]) && numbers.contains(&board[3][0]) && numbers.contains(&board[4][0])) ||
+		(numbers.contains(&board[0][1]) && numbers.contains(&board[1][1]) && numbers.contains(&board[2][1]) && numbers.contains(&board[3][1]) && numbers.contains(&board[4][1])) ||
+		(numbers.contains(&board[0][2]) && numbers.contains(&board[1][2]) && numbers.contains(&board[2][2]) && numbers.contains(&board[3][2]) && numbers.contains(&board[4][2])) ||
+		(numbers.contains(&board[0][3]) && numbers.contains(&board[1][3]) && numbers.contains(&board[2][3]) && numbers.contains(&board[3][3]) && numbers.contains(&board[4][3])) ||
+		(numbers.contains(&board[0][4]) && numbers.contains(&board[1][4]) && numbers.contains(&board[2][4]) && numbers.contains(&board[3][4]) && numbers.contains(&board[4][4]))
+	{
+		// Verticals.
+	}
+	else
+	{
+		return -1;
+	}
+	
+	// Sum the board.
+	let mut sum = 0;
+	for i in 0..5
+	{
+		for j in 0..5
+		{
+			if (!numbers.contains(&board[i][j]))
+			{
+				sum = sum + board[i][j];
+			}
+		}
+	}
+	
+	sum
+}
+
 fn main() -> std::io::Result<()>
 {
 	let mut input = String::new();
