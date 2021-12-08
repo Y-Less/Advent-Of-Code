3efecc222df45e9d38c857c7a3f98bf6998955a7
Y_Less <git-athena@y-less.com>
2021-12-08 21:37:24 +0000

    Generate the grid.

--- 2021/d05a/src/main.rs
+++ 2021/d05a/src/main.rs
@@ -14,14 +14,57 @@ fn main() -> std::io::Result<()>
 	let sep = Regex::new(r"\s+->\s+|,").unwrap();
 	let mut vents: HashMap<i32, HashMap<i32, i32>> = HashMap::new();
 	let mut lines = input.trim().split('\n');
+	let mut dangerous = 0;
 	for line in lines
 	{
 		let parts = sep.split(line).map(|x| x.parse().unwrap()).collect::<Vec<i32>>();
-		println!("{:?}", parts);
+		if (parts[0] != parts[2] && parts[1] != parts[3])
+		{
+			continue;
+		}
+		println!("{:?}", line);
+		for i in std::cmp::min(parts[0], parts[2])..std::cmp::max(parts[0], parts[2]) + 1
+		{
+			for j in std::cmp::min(parts[1], parts[3])..std::cmp::max(parts[1], parts[3]) + 1
+			{
+				//println!("{:?} {:?}", i, j);
+				if let Some(x) = vents.get_mut(&i)
+				{
+					if (x.contains_key(&j))
+					{
+						if (x[&j] == 1)
+						{
+							dangerous = dangerous + 1;
+						}
+						x.insert(j, x[&j] + 1);
+					}
+					else
+					{
+						x.insert(j, 1);
+					}
+				}
+				else
+				{
+					let mut v = HashMap::new();
+					v.insert(j, 1);
+					vents.insert(i, v);
+				}
+				//if (!vents.contains_key(&i))
+				//{
+				//	vents[&i].insert(HashMap::new());
+				//}
+				//if (vents[&i].contains_key(&j))
+				//{
+				//	vents[&i][&j].insert(vents[&i][&j] + 1);
+				//}
+				//else
+				//{
+				//	vents[&i][&j] = 1;
+				//}
+			}
+		}
 	}
-		//.map(|x| x.parse().unwrap())
-		//.collect::<&str>();
-
+	//println!("{:?}", vents);
 
 	//lines.next();
 	//let mut board: [[i32; 5]; 5] = [[0; 5]; 5];
