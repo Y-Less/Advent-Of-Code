562fa0763098d94994618d9a977a6b911de3d96b
Y_Less <git-athena@y-less.com>
2022-12-04 19:28:48 +0000

    Deleted some old code.

--- 2022/d04a/src/main.rs
+++ 2022/d04a/src/main.rs
@@ -1,29 +1,6 @@
 use std::fs::File;
 use std::io::prelude::*;
 
-fn bitmap(s: &str) -> (u64)
-{
-	let mut ret: u64 = 0;
-	let len = s.len();
-	for i in 0 .. len
-	{
-		{
-			// Apparently `nth` removes the data...
-			let j = s.bytes().nth(i);
-			let b = j.unwrap() - 0x41; // - 'A'
-			if b >= 26
-			{
-				ret = ret | (1u64 << b - 32);
-			}
-			else
-			{
-				ret = ret | (1u64 << b + 26);
-			}
-		}
-	}
-	ret
-}
-
 fn main() -> std::io::Result<()>
 {
 	let mut input = String::new();
