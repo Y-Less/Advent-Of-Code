4693fe0ae20f45fd07fd5d35645401b467011d74
Y_Less <git-athena@y-less.com>
2022-12-04 19:30:00 +0000

    Day 4b.

---
+++ 2022/d04b/src/main.rs
@@ -0,0 +1,39 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() -> std::io::Result<()>
+{
+	let mut input = String::new();
+	{
+		let mut file = File::open("../inputs/d04.txt")?;
+		file.read_to_string(&mut input)?;
+	}
+
+	let lines = input.trim().split('\n');
+	let mut total = 0;
+	for line in lines
+	{
+		let pairs = line.trim().split(',').collect::<Vec<&str>>();
+		let l = pairs[0].split('-')
+			.map(|x| x.parse().expect("Not a number"))
+			.collect::<Vec<i32>>();
+		let r = pairs[1].split('-')
+			.map(|x| x.parse().expect("Not a number"))
+			.collect::<Vec<i32>>();
+		if l[0] <= r[0] && l[1] >= r[1]
+		{
+			//println!("{:?} is fully contained in {:?}", r, l);
+			total = total + 1;
+		}
+		else if r[0] <= l[0] && r[1] >= l[1]
+		{
+			//println!("{:?} is fully contained in {:?}", l, r);
+			total = total + 1;
+		}
+	}
+	
+	println!("{:?}", total);
+
+	Ok(())
+}
+
