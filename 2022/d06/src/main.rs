5cb31f528b7801d144f269fa550bee8ba8aee599
Y_Less <git-athena@y-less.com>
2022-12-09 18:59:39 +0000

    PArt A done.

--- 2022/d06/src/main.rs
+++ 2022/d06/src/main.rs
@@ -5,13 +5,30 @@ fn main() -> std::io::Result<()>
 {
 	let mut input = String::new();
 	{
-		let mut file = File::open("../inputs/d06.txt")?;
+		let mut file = File::open("input.txt")?;
 		file.read_to_string(&mut input)?;
 	}
 
-	for ch in input
+	let mut idx = 0;
+	let mut a = ' ';
+	let mut b = ' ';
+	let mut c = ' ';
+	let mut d = ' ';
+	for ch in input.chars()
 	{
-		println!("{:?}", ch);
+		idx = idx + 1;
+		a = b;
+		b = c;
+		c = d;
+		d = ch;
+		if (idx >= 4)
+		{
+			if (a != b && a != c && a != d && b != c && b != d && c != d)
+			{
+				println!("Part A: {:?} {:?} {:?} {:?} {:?}", idx, a, b, c, d);
+				break;
+			}
+		}
 	}
 
 	Ok(())
