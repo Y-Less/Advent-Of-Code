8337fac7f8bd05a02cb80f0583e84a3113cfc1ab
Y_Less <git-athena@y-less.com>
2022-12-09 21:09:25 +0000

    Trying to use lifetimes.

--- 2022/d07/src/main.rs
+++ 2022/d07/src/main.rs
@@ -1,14 +1,15 @@
 use std::fs::File;
 use std::io::prelude::*;
 
-struct Entry
+struct Entry<'b>
 {
+	name: &'b str,
 	isDir: bool,
 	size: usize,
-	entries: Vec<Entry>,
+	entries: Vec<Entry<'b>>,
 }
 
-impl Entry
+impl<'b> Entry<'b>
 {
 	fn getSize(&self) -> usize
 	{
@@ -23,12 +24,50 @@ impl Entry
 	}
 }
 
-fn listDir<'a>(name: &str, mut lines: impl std::iter::Iterator<Item = &'a str>) -> Entry
+fn listDir<'a, 'b>(name: &str, mut lines: impl std::iter::Iterator<Item = &'a str>) -> Entry<'b>
 {
 	//let x = lines.next();
 	//println!("{:?}", x);
+	let mut entries: Vec<Entry<'b>> = vec!();
+	
+	for line in lines
+	{
+		if line == "$ ls"
+		{
+			// Ignore this.
+			println!("ls");
+		}
+		else if line == "$ cd .."
+		{
+			println!("back");
+			break;
+		}
+		if line.chars().nth(0) == Some('$')
+		{
+			println!("{:?}", line.get(5..));
+		}
+		else if line.get(0..4) == Some("dir ")
+		{
+			// Add an entry.
+			println!("dir: {:?}", line.get(4..));
+		}
+		else
+		{
+			// Add an entry.
+			println!("{:?}", line);
+			let bits = line.split(' ');
+			entries.push(Entry {
+				name: bits.nth(1).unwrap(),
+				size: bits.nth(0).unwrap().parse::<usize>().expect("Not a number"),
+				isDir: false,
+				entries: vec!(),
+			});
+		}
+	}
+	
 	Entry {
-		isDir: false,
+		name,
+		isDir: true,
 		size: 0,
 		entries: vec!(),
 	}
