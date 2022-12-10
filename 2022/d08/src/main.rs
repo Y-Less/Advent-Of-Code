96b451c41a8ee7e86b5a349e573bc25c954efc2b
Y_Less <git-athena@y-less.com>
2022-12-10 11:13:23 +0000

    One row of visibility.

--- 2022/d08/src/main.rs
+++ 2022/d08/src/main.rs
@@ -11,9 +11,26 @@ fn main() -> std::io::Result<()>
 		vec!(3, 5, 3, 9, 0)
 	);
 
-	let visibility = vec![vec![0; input[0].len()]; input.len()];
+	let w = input.len();
+	let h = input[0].len();
+	let mut visibility = vec![vec![0; h]; w];
     
 	println!("Debug A: {:?}", input);
+	
+	for i in 0..w
+	{
+		let mut highest = -1;
+		for j in 0..h
+		{
+			let cur = input[i][j];
+			if cur > highest
+			{
+				highest = cur;
+				visibility[i][j] = visibility[i][j] | 1;
+			}
+		}
+	}
+	
 	println!("Debug A: {:?}", visibility);
 
 	Ok(())
